12d0dbfedd1daae5096e0439960391f135db4fc5
Andy Nowacki <a.nowacki@leeds.ac.uk>
2018-09-11 21:29:38 +0100

    Add sphere option to geometric functions
    Use sphere=true to force flattening to be 0 for calculations,
    which may be useful when dealing with synthetics on a sphere.
    
    Tests are still needed for the geometric functions.

--- src/geometry.jl
+++ src/geometry.jl
@@ -1,19 +1,22 @@
+# Event-station geometry calculations on a (maybe flattened) sphere
+
 """
-    azimuth(trace; flattening=Geodesics.F_WGS84) -> az
-    azimuth(event, station; flattening=Geodesics.F_WGS84) -> az
+    azimuth(trace; sphere=false, flattening=Geodesics.F_WGS84) -> az
+    azimuth(event, station; sphere=false, flattening=Geodesics.F_WGS84) -> az
 
 Return the azimuth `az` from the event to the station (a seismic station) in
 degrees east from local north at the event for a `trace`.  Alternatively specify
 the `event` and `station` individually.
 
 Optionally specify the `flattening` of the ellipsoid of rotation on which this
-is computed, which defaults to that of the WGS84 ellipsoid.
+is computed, which defaults to that of the WGS84 ellipsoid.  If `sphere` is `true`,
+then `flattening` is set to zero and the calculation is performed on a sphere.
 """
-function azimuth(e::Event, s::Station; flattening=Geodesics.F_WGS84)
+function azimuth(e::Event, s::Station; sphere=false, flattening=Geodesics.F_WGS84)
     _check_headers_geometry(e, s)
-    Geodesics.azimuth(e.lon, e.lat, s.lon, s.lat, true, f=flattening)
+    Geodesics.azimuth(e.lon, e.lat, s.lon, s.lat, true, f=sphere ? 0.0 : flattening)
 end
-azimuth(t::Trace, args...) = azimuth(t.evt, t.sta, args...)
+azimuth(t::Trace, args...; kwargs...) = azimuth(t.evt, t.sta, args...; kwargs...)
 
 """
     backazimuth(trace; flattening=$(Geodesics.F_WGS84)) -> baz
@@ -24,28 +27,30 @@ degrees east from local north at the station for a `trace`.  Alternatively speci
 the `station` and `event` individually.
 
 Optionally specify the `flattening` of the ellipsoid of rotation on which this
-is computed, which defaults to that of the WGS84 ellipsoid.
+is computed, which defaults to that of the WGS84 ellipsoid.  If `sphere` is `true`,
+then `flattening` is set to zero and the calculation is performed on a sphere.
 """
-function backazimuth(s::Station, e::Event; flattening=Geodesics.F_WGS84)
+function backazimuth(s::Station, e::Event; sphere=false, flattening=Geodesics.F_WGS84)
     _check_headers_geometry(e, s)
-    Geodesics.azimuth(s.lon, s.lat, e.lon, e.lat, true, f=flattening)
+    Geodesics.azimuth(s.lon, s.lat, e.lon, e.lat, true, f=(sphere ? 0.0 : flattening))
 end
 backazimuth(t::Trace, args...; kwargs...) = backazimuth(t.sta, t.evt, args...; kwargs...)
 
 """
-    distance_deg(trace; flattening=$(Geodesics.F_WGS84)) -> Δ
-    distance_deg(station, event; flattening=$(Geodesics.F_WGS84)) -> Δ
-    distance_deg(event, station; flattening=$(Geodesics.F_WGS84)) -> Δ
+    distance_deg(trace; sphere=false, flattening=$(Geodesics.F_WGS84)) -> Δ
+    distance_deg(station, event; sphere=false, flattening=$(Geodesics.F_WGS84)) -> Δ
+    distance_deg(event, station; sphere=false, flattening=$(Geodesics.F_WGS84)) -> Δ
 
 For a `trace` or an `event`-`station` pair, return the epicentral angular distance
 `Δ` in degrees.
 
 Optionally specify the `flattening` of the ellipsoid of rotation on which this
-is computed, which defaults to that of the WGS84 ellipsoid.
+is computed, which defaults to that of the WGS84 ellipsoid.  If `sphere` is `true`,
+then `flattening` is set to zero and the calculation is performed on a sphere.
 """
-function distance_deg(e::Event, s::Station; flattening=Geodesics.F_WGS84)
+function distance_deg(e::Event, s::Station; sphere=false, flattening=Geodesics.F_WGS84)
     _check_headers_geometry(e, s)
-    Geodesics.angular_distance(e.lon, e.lat, s.lon, s.lat, true, f=flattening)
+    Geodesics.angular_distance(e.lon, e.lat, s.lon, s.lat, true, f=(sphere ? 0.0 : flattening))
 end
 distance_deg(s::Station, e::Event, args...; kwargs...) = distance_deg(e, c, args...; kwargs...)
 distance_deg(t::Trace, args...; kwargs...) = distance_deg(t.evt, t.sta, args...; kwargs...)
@@ -58,12 +63,14 @@ in km.
 
 Optionally specify the semimajor radius `a` in km and `flattening` of the
 ellipsoid of rotation on which this is computed, which defaults to that of the
-WGS84 ellipsoid.
+WGS84 ellipsoid.    If `sphere` is `true`, then `flattening` is set to zero and
+the calculation is performed on a sphere.
 """
-function distance_km(e::Event, s::Station; a=Geodesics.EARTH_R_MAJOR_WGS84/1e3,
+function distance_km(e::Event, s::Station; sphere=false, a=Geodesics.EARTH_R_MAJOR_WGS84/1e3,
               flattening=Geodesics.F_WGS84)
     _check_headers_geometry(e, s)
-    Geodesics.surface_distance(e.lon, e.lat, s.lon, s.lat, a, true, f=flattening)
+    Geodesics.surface_distance(e.lon, e.lat, s.lon, s.lat, a, true,
+                               f=(sphere ? 0.0 : flattening))
 end
 distance_km(s::Station, e::Event; kwargs...) = distance_km(e, s; kwargs...)
 distance_km(t::Trace; kwargs...) = distance_km(t.evt, t.sta; kwargs...)
