e1a00a6835c88224ea070da247e82ff9dfdeedc3
Andy Nowacki <a.nowacki@leeds.ac.uk>
2018-11-02 20:27:11 +0000

    Rename test files
    Remove the prefix "test_" from all the test files, as this is
    redundant.
    
    Retain the test_data directory.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,12 @@ using Test
 using Seis
 
 @testset "Seis" begin
-    include("test_filtering.jl")
-    include("test_io.jl")
-    include("test_operations.jl")
-    include("test_sample_data.jl")
-    include("test_synth.jl")
-    include("test_traveltimes.jl")
-    include("test_types.jl")
-    include("test_util.jl")
+    include("filtering.jl")
+    include("io.jl")
+    include("operations.jl")
+    include("sample_data.jl")
+    include("synth.jl")
+    include("traveltimes.jl")
+    include("types.jl")
+    include("util.jl")
 end
