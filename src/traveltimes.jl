0a902b7c1c335229935b78349d7b56a88b3b4466
Andy Nowacki <andy.nowacki@bristol.ac.uk>
2020-06-02 20:54:11 +0100

    Improve docstrings for travel time functions

--- src/traveltimes.jl
+++ src/traveltimes.jl
@@ -15,7 +15,7 @@ If `name` is missing, then the pick is added to a numbered set of picks.
 (Direct manipulation of picks is easy: just do `t.picks.PKP = (1001, "PKP")` to set
 a picks with name "PKP", time 1001 s and key `:PKP`.)
 
-### Example:
+# Example
 
 ```julia
 julia> t = Trace(0, 1, 2);
@@ -43,7 +43,7 @@ julia> t.picks[1]
 Seis.Pick{Float64}((time=2.0, name=missing))
 ```
 """
-function add_pick!(t::AbstractTrace, time, name::Union{Missing,AbstractString}=missing)
+function add_pick!(t::AbstractTrace, time, name=missing)
     p = (time=time, name=name)
     key = if !ismissing(name)
         base_key = name
@@ -70,16 +70,52 @@ end
 
 Add a travel time pick to the `Trace` `t` from a `Seis.Pick`.  By default,
 the pick name is used.
+
+# Example
+```
+julia> t1 = Trace(0, 1, 20); t2 = sample_data();
+
+julia> add_pick!(t1, t2.picks.A, "A")
+Seis.Pick{Float64}((time=53.67000198364258, name="A"))
+
+julia> t1.picks
+Seis.SeisDict{Union{Int64, Symbol},NamedTuple{(:time, :name),Tuple{Float64,Union{Missing, String}}}} with 1 entry:
+  :A => Seis.Pick{Float64}((time=53.67000198364258, name="A"))
+```
 """
 add_pick!(t::AbstractTrace, p::Pick, name=p.name) = add_pick!(t, p.time, name)
 
 # Stub to allow methods to be added using a travel time package (like SeisTau)
+"""
+    add_picks!
+
+Add picks to traces based on seismic phases' predicted arrival time.
+
+Seis does not itself implement seismic phase travel time computation.
+See [SeisTau](https://github.com/anowacki/SeisTau.jl) for one implementation
+which adds methods to `add_pick!`.
+"""
 function add_picks! end
 
 """
     clear_picks!(t)
 
 Remove all picks associated with the `Trace` `t`.
+
+# Example
+```
+julia> t = sample_data();
+
+julia> picks(t)
+2-element Array{NamedTuple{(:time, :name),Tuple{Float32,Union{Missing, String}}},1}:
+ Seis.Pick{Float32}((time=53.670002, name=missing))
+ Seis.Pick{Float32}((time=60.980003, name=missing))
+
+julia> clear_picks!(t);
+
+julia> picks(t)
+0-element Array{NamedTuple{(:time, :name),Tuple{Float32,Union{Missing, String}}},1}
+```
 """
 clear_picks!(t::AbstractTrace) = empty!(t.picks)
 
@@ -115,6 +151,24 @@ which either are exactly `name` or match the regular expression `pattern`.
 
 By default, picks are returned in order of increasing time.  Use `sort=:name`
 to sort alphanumerically by name (where unnamed picks appear first).
+
+# Example
+```
+julia> t = Trace(0, 1, 2); t.picks.P = (1, "Pn"); t.picks.S = (1.8, "S");
+
+julia> t.picks
+Seis.SeisDict{Union{Int64, Symbol},NamedTuple{(:time, :name),Tuple{Float64,Union{Missing, String}}}} with 2 entries:
+  :P => Seis.Pick{Float64}((time=1.0, name="Pn"))
+  :S => Seis.Pick{Float64}((time=1.8, name="S"))
+
+julia> picks(t, "S")
+1-element Array{NamedTuple{(:time, :name),Tuple{Float64,Union{Missing, String}}},1}:
+ Seis.Pick{Float64}((time=1.8, name="S"))
+
+julia> picks(t, r"^P.*")
+1-element Array{NamedTuple{(:time, :name),Tuple{Float64,Union{Missing, String}}},1}:
+ Seis.Pick{Float64}((time=1.0, name="Pn"))
+```
 """
 function picks(t::AbstractTrace, name_or_match; sort=:time)
     ps = _picks(t, name_or_match)
