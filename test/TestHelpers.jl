195357d455f0f8162040daeca7b3e29b40c2892e
Andy Nowacki <a.nowacki@leeds.ac.uk>
2021-10-18 12:28:08 +0100

    TestHelpers: Add methods for creating randomly-oriented traces
    Add methods to `random_basis_traces` taking in a full `Trace` type,
    and using this to infer the geometry and element type.
    
    Also make comments regarding rounding clearer.

--- test/TestHelpers.jl
+++ test/TestHelpers.jl
@@ -34,24 +34,30 @@ azimuth_inclination(v) = atand(v[1], v[2]), acosd(v[3])
 "Get a unit vector from an azimuth and inclination"
 unit_vector(azi, inc) = @SVector[sind(azi)*sind(inc), cosd(azi)*sind(inc), cosd(inc)]
 
-"Create a randomly-oriented set of traces making up a right-handed set"
+"""
+Create a randomly-oriented set of traces making up a right-handed set,
+with 
+"""
 function random_basis_traces(T=Float64, P=Seis.Geographic{T})
     x, y, z = [Trace{T, Vector{T}, P}(0, 1, 0) for _ in 1:3]
     x.sta.cha, y.sta.cha, z.sta.cha = "X", "Y", "Z"
     # Do vector creation and calculation in Float64
-    # A random unit vector give the first trace's direction
+    # A random unit vector gives the first trace's direction
     ux = random_unit_vector(Float64)
     # Then find an orthogonal vector
     v = random_unit_vector(Float64)
     uy = normalize(ux × v)
     # Third direction gives right-handed set x, y, z
     uz = ux × uy
-    # Only round to lower precision at this point
+    # Only round from Float64 to lower precision at this point during
+    # auto-conversion to T
     x.sta.azi, x.sta.inc = azimuth_inclination(ux)
     y.sta.azi, y.sta.inc = azimuth_inclination(uy)
     z.sta.azi, z.sta.inc = azimuth_inclination(uz)
     x, y, z
 end
+random_basis_traces(::Type{Trace{T}}) where T = random_basis_traces(T, Seis.Geographic{T})
+random_basis_traces(::Type{CartTrace{T}}) where T = random_basis_traces(T, Seis.Cartesian{T})
 
 "Return a vector and one randomly deviated away from it by `θ`°."
 function vector_and_deviated_vector(T, θ)
